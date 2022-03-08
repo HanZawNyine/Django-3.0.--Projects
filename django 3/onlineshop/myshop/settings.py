3b80e4da671733b8f8e0976cb9185500a766c56b
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-08 20:12:06 +0630

    social app

--- django 3/onlineshop/myshop/settings.py
+++ django 3/onlineshop/myshop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
